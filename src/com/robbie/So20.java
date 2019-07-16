bc351d66811a4cb25536201399d4b54ad8c19d76
liyunpeng <lyplover@gmail.com>
2019-07-16 14:27:30 +0800

    So 20

---
+++ src/com/robbie/So20.java
@@ -0,0 +1,52 @@
+package com.robbie;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class So20 {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if (s == null || s.length() == 0)
+            return true;
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        stack.push(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty()) {
+                if (map.containsKey(c))
+                    stack.push(c);
+                else
+                    return false;
+            } else {
+                char top = stack.peek();
+                if (map.containsKey(top)) {
+                    if (map.containsKey(c)) {
+                        stack.push(c);
+                        continue;
+                    }
+                    if (map.get(top) == c)
+                        stack.pop();
+                    else
+                        return false;
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        So20 so = new So20();
+        System.out.println(so.isValid("()"));
+        System.out.println(so.isValid("()[]{}"));
+        System.out.println(so.isValid("(]"));
+        System.out.println(so.isValid("([)]"));
+        System.out.println(so.isValid("{[]}"));
+    }
+}
