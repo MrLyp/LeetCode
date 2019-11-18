db07f31ecc5e0c77905970fec587631df032635e
liyunpeng <lyplover@gmail.com>
2019-11-18 17:11:57 +0800

    307

--- src/com/robbie/So307.java
+++ src/com/robbie/So307.java
@@ -1,13 +1,15 @@
 package com.robbie;
 
+import java.util.Arrays;
+
 public class So307 {
-    static class NumArray {
+    static class NumArrayBrute {
 
         private int[] sum;
 
         private int[] nums;
 
-        public NumArray(int[] nums) {
+        public NumArrayBrute(int[] nums) {
             this.nums = nums;
             sum = new int[nums.length];
             if (nums.length == 0)
@@ -30,10 +32,71 @@ public class So307 {
         }
     }
 
+    static class NumArray {
+
+        private int[] array;
+
+        private int[] nums;
+
+        public NumArray(int[] nums) {
+            if (nums == null || nums.length == 0)
+                return;
+            this.nums = nums;
+            int n = nums.length;
+            array = new int[n * 4];
+            build(nums, 0, n - 1, 0);
+            System.out.println(Arrays.toString(array));
+        }
+
+        private void build(int[] nums, int start, int end, int index) {
+            if (start > end)
+                return;
+            if (start == end) {
+                array[index] = nums[start];
+                return;
+            }
+            int mid = (start + end) / 2;
+            build(nums, start, mid, index * 2 + 1);
+            build(nums, mid + 1, end, index * 2 + 2);
+            array[index] = array[2 * index + 1] + array[2 * index + 2];
+        }
+
+        public void update(int i, int val) {
+            update(0, nums.length - 1, 0, i, val);
+        }
+
+        private void update(int start, int end, int index, int i, int val) {
+            if (start == end) {
+                array[index] = val;
+                return;
+            }
+            int mid = (start + end) / 2;
+            if (i <= mid)
+                update(start, mid, index * 2 + 1, i, val);
+            else
+                update(mid + 1, end, index * 2 + 2, i, val);
+            array[index] = array[2 * index + 1] + array[2 * index + 2];
+        }
+
+        public int sumRange(int i, int j) {
+            return sumRange(0, nums.length - 1, i, j, 0);
+        }
+
+        private int sumRange(int start, int end, int i, int j, int index) {
+            if (start > j || end < i)
+                return 0;
+            if (i <= start && end <= j) {
+                return array[index];
+            }
+            int mid = (start + end) / 2;
+            return sumRange(start, mid, i, j, index * 2 + 1) + sumRange(mid + 1, end, i, j, index * 2 + 2);
+        }
+    }
+
     public static void main(String[] args) {
-        NumArray array = new NumArray(new int[]{1,3,5});
+        NumArray array = new NumArray(new int[]{1, 3, 5});
         System.out.println(array.sumRange(0, 2));
-        array.update(1,2);
+        array.update(1, 2);
         System.out.println(array.sumRange(0, 2));
     }
 }
